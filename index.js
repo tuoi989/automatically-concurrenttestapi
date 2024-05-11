5fc3caefed9ab7c2daccd090a552a910247c3534
tuoi989 <jlourenco2pun0suy@hotmail.com>
2024-05-11 16:36:48 +0700

    feat task

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else right = mid;
+  }
+  return nums[left];
+}
