06aa3e8aeca7607995b5e3d7b557ccb6f5a44079
tuoi989 <jlourenco2pun0suy@hotmail.com>
2024-05-12 22:05:39 +0700

    hello

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else right = mid;
-  }
-  return nums[left];
+function getMin(arr) {
+  return Math.min(...arr);
 }
